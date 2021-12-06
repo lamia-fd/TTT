fc59285c6eb34970256138b969a4da8e37de474d
lamia <lamia.fd123@gmail.com>
2021-12-06 13:04:05 +0300

    v1

--- ttt/ViewController.swift
+++ ttt/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet var btn3: [UIButton]!
 
-
+    @IBAction func resat(_ sender: UIButton) {
+        for bt in btn3{
+            bt.layer.backgroundColor = UIColor.gray.cgColor
+            count = 0
+        }
+    }
+    @IBAction func btn1(_ sender: UIButton) {
+       
+        if sender.layer.backgroundColor == UIColor.red.cgColor ||             sender.layer.backgroundColor == UIColor.blue.cgColor
+{
+//do nothing
+        }
+        else if count % 2 == 0{
+            count += 1
+            sender.layer.backgroundColor = UIColor.red.cgColor
+        }else{
+            count += 1
+            sender.layer.backgroundColor = UIColor.blue.cgColor
+        }
+    }
+   
+    
+    
+    
 }
 
